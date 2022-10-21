3697d549433d5bee11fa0651c26dc9ad989cb945
Amrita2125 <116201089+Amrita2125@users.noreply.github.com>
2022-10-21 18:37:45 +0530

    Create Enter Name , Age and Number.cpp

---
+++ CPP/Enter Name , Age and Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+string name;
+int age,num;
+cout<<"Enter your name : "; 
+cin>>name;
+cout<<"Enter your age : "; 
+cin>>age;
+cout<<"Enter your num : "; 
+cin>>num;
+cout<<"Name that you entered :";
+cout<<name<<endl;
+cout<<"Age that you entered :";
+cout<<age<<endl;
+cout<<"Number that you entered :";
+cout<<num<<endl;
+
+}
