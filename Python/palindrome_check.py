e400474f10d34ad39ee331ff9995c00ffbce7b82
VIKRANT SINGH <vikrantsingh17565@gmail.com>
2023-10-05 20:19:05 +0530

    Added code for Palindrome Check in python

--- Python/palindrome_check.py
+++ Python/palindrome_check.py
@@ -1,4 +1,4 @@
-def check_pelindrome(string):
+def check_palindrome(string):
     for i in range(len(string)//2):
         if string[i] != string[len(string)-i-1]:
             return False
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     
     s = str(input())
 
-    if(check_pelindrome(s)):
-        print(s, "is a pelindrome")
+    if(check_palindrome(s)):
+        print(s, "is a palindrome")
     else:
-        print(s, "is not a pelindrome")
+        print(s, "is not a palindrome")
