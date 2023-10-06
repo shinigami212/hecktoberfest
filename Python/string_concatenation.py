71a18d5aaa0cf9ba452410e0f0f976769f928828
VIKRANT SINGH <vikrantsingh17565@gmail.com>
2023-10-06 13:10:10 +0530

    Added: String Concatenation: Concatenate two strings

---
+++ Python/string_concatenation.py
@@ -0,0 +1,11 @@
+# String Concatenation: Concatenate two strings
+
+def concatenate(str1, str2):
+    return str1 + str2
+
+if __name__ == "__main__":
+    
+    str1 = str(input())
+    str2 = str(input())
+
+    print(concatenate(str1, str2))
