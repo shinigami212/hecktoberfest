a329757b8547947865cf3deebc7c09b452fe1434
Tanishaclippy <127332784+Tanishaclippy@users.noreply.github.com>
2023-10-07 11:56:19 +0530

    Create Sumoftwonumbers.java

---
+++ Sumoftwonumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	  System.out.println("Enter the number 1");
+	  double n1=sc.nextDouble();
+	  System.out.println("Enter the number 2");
+	  double n2=sc.nextDouble();
+	  System.out.println("The sum of the two numbers is "+(n1+n2));
+	
+	}
+}
