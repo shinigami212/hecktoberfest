0641ecb8aa43633edd6c0e0675e2d8f104131395
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2023-10-10 09:45:02 +0530

    Update fibonacci.cpp

--- c++/fibonacci.cpp
+++ c++/fibonacci.cpp
@@ -8,11 +8,11 @@ int main(){
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
     ll n;
-    cout << "Enter the number of terms of fibonacci sequence you want: "
+    cout << "Enter the number of terms of fibonacci sequence you want: ";
     cin >> n;
     ll prev= 1, prevprev = 0;
     cout << "0 1 ";
-    for(int i = 2; i <= n; i++){
+    for(int i = 2; i < n; i++){
         ll j = prev + prevprev;
         cout << j << " ";
         prevprev = prev;
